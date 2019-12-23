8e84664a8389b3eb88e15f59c894e5f3e453f0fb
codermarcos <coder.marcos@gmail.com>
2019-12-23 15:22:20 -0300

    :new: Add typings npm

--- types/simple-mask-money.d.ts
+++ types/simple-mask-money.d.ts
@@ -1,26 +1,26 @@
-interface ISimpleMaskMoneyArgs {
-  afterFormat: (e: string) => {}; 
-  allowNegative: boolean = false;  
-  beforeFormat: (e: string) => {};  
-  negativeSignAfter: boolean = false;
-  decimalSeparator: string = ',';
-  fixed: boolean = true;
-  fractionDigits: number = 2;
-  prefix: string;
-  suffix: string;
-  thousandsSeparator: string = '.';
-  cursor: 'start' | 'move' | 'end';
+export interface ISimpleMaskMoneyArgs {
+  afterFormat?: (e: string) => {}; 
+  allowNegative?: boolean | false;  
+  beforeFormat?: (e: string) => {};  
+  negativeSignAfter?: boolean | false;
+  decimalSeparator?: string | ',';
+  fixed?: boolean | true;
+  fractionDigits?: number | 2;
+  prefix?: string | '';
+  suffix?: string | '';
+  thousandsSeparator?: string | '.';
+  cursor?: 'start' | 'move' | 'end';
 }
 
-interface ISimpleMaskMoney {
+declare class SimpleMaskMoney {
   public static args: ISimpleMaskMoneyArgs;
-  public static formatToCurrency(value: number, args?: ISimpleMaskMoneyArgs): string;
-  public static formatToMask(value: number, args?: ISimpleMaskMoneyArgs): string;
-  public static formatToNumber(value: string, args?: ISimpleMaskMoneyArgs): number;
+  public static formatToCurrency(value: number | string, args?: ISimpleMaskMoneyArgs): string;
+  public static formatToMask(value: number | string, args?: ISimpleMaskMoneyArgs): string;
+  public static formatToNumber(value: string | number, args?: ISimpleMaskMoneyArgs): number;
   public static setMask(element: string | HTMLInputElement, args?: ISimpleMaskMoneyArgs): HTMLInputElement;
 }
 
 declare module 'simple-mask-money' {
-  export = ISimpleMaskMoney;
+  export = SimpleMaskMoney;
 }
 
